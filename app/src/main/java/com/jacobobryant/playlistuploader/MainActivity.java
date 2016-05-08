a7b3d2763eba46570d588ad37837fd990f557978
Jacob O'Bryant <obryant666@gmail.com>
2016-05-07 23:12:50 -0600

    Set up queries for playlist songs

--- app/src/main/java/com/jacobobryant/playlistuploader/MainActivity.java
+++ app/src/main/java/com/jacobobryant/playlistuploader/MainActivity.java
@@ -1,12 +1,83 @@
 package com.jacobobryant.playlistuploader;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
     public static String TAG = "PlaylistUploader";
+    public static final int PLAYLIST_LOADER = 0;
+    public static final int SONG_LOADER = 1;
+    public static final String KEY_ID = "id";
+
+    private class PlaylistLoader implements LoaderManager.LoaderCallbacks<Cursor> {
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int loaderID, Bundle bundle) {
+            final String[] PLAYLIST_PROJ = {
+                MediaStore.Audio.Playlists._ID
+            };
+            return new CursorLoader(MainActivity.this,
+                        MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI,
+                        PLAYLIST_PROJ,
+                        null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+            Log.d(TAG, "Got " + cursor.getCount() + " playlists");
+            cursor.moveToPosition(-1);
+            int songLoaderId = SONG_LOADER;
+            LoaderManager.LoaderCallbacks<Cursor> songLoader = new SongLoader();
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(0);
+                Log.d(TAG, "playlist id: " + id);
+                Bundle args = new Bundle();
+                args.putInt(KEY_ID, id);
+                getSupportLoaderManager().initLoader(songLoaderId++, args, songLoader);
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) { }
+    }
+
+    private class SongLoader implements LoaderManager.LoaderCallbacks<Cursor> {
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int loaderID, Bundle bundle) {
+            int playlistId = bundle.getInt(KEY_ID);
+            final String[] SONG_PROJ = {
+                MediaStore.Audio.Playlists.Members.AUDIO_ID,
+                MediaStore.Audio.Playlists.Members.TITLE,
+                MediaStore.Audio.Playlists.Members.ARTIST,
+                MediaStore.Audio.Playlists.Members.ALBUM
+            };
+            return new CursorLoader(MainActivity.this,
+                        MediaStore.Audio.Playlists.Members.getContentUri("external", playlistId),
+                        SONG_PROJ,
+                        null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+            Log.d(TAG, "Got " + cursor.getCount() + " songs");
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) { }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +86,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void upload(View v) {
-        Log.d(TAG, "hello there!");
+        getSupportLoaderManager().initLoader(PLAYLIST_LOADER, null, new PlaylistLoader());
     }
 }
